567fc187d73b2ea4e978feaa4ad22cbc47cd46ae
AlessioBucheri <157613253+AlessioBucheri@users.noreply.github.com>
2024-06-29 12:44:21 +0200

    edit green gourmet data

--- src/Database/projectsData.js
+++ src/Database/projectsData.js
@@ -2,14 +2,14 @@ const projectsData = [
     {
         id: 1,
         title: "Footprint Travel",
-        description: "<p><strong>Flight Carbon Footprint Calculator</strong></p><p><strong>Project</strong>: Final exam for the Frontend Developer course at Start2Impact</p><p><strong>Technologies</strong>: React, API</p><p><strong>Description</strong>: Web app that calculates the carbon footprint of flights.</p><p><strong>Main challenge</strong>: implementing efficient API calls for the app's functionality and dynamic airport selection with real-time suggestions while typing.</p>",
+        description: "<p><strong>Flight Carbon Footprint Calculator</strong></p><p><strong>Project</strong>: Final exam for the Frontend Developer course at Start2Impact</p><p><strong>Technologies</strong>: HTML, CSS, JS, React, API</p><p><strong>Description</strong>: Web app that calculates the carbon footprint of flights.</p><p><strong>Main challenge</strong>: implementing efficient API calls for the app's functionality and dynamic airport selection with real-time suggestions while typing.</p>",
         link: "https://footprinttravel.netlify.app/",
         image: "/ProjectImage/footprintTravel.png"
     },
     {
         id: 2,
         title: "GreenGourmet",
-        description: "Description for project two",
+        description: "<p><strong>Vegan Recipes for everyone</strong></p><p><strong>Project</strong>: Final exam for React course at Start2Impact</p><p><strong>Technologies</strong>: HTML, CSS, JS, React, API</p><p><strong>Description</strong>: This webapp allows you to search for vegan recipes, starting from the ingredients you want.</p><p><strong>Main challenge</strong>: This was my first complete React app, so the real challenge was understand how works react and how to use it.</p>",
         link: "https://greengourmet.netlify.app/",
         image: "/ProjectImage/greengourmet.png"
     },
